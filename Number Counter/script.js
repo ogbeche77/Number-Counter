2ba5d4472c0ed6e019c2cbf8e07b45eef1bbe7c6
ogbeche77 <ogbeche77@yahoo.com>
2020-04-23 13:10:12 +0200

    dom

--- Number Counter/script.js
+++ Number Counter/script.js
@@ -7,28 +7,30 @@ var MrCount = 0;
 plusCount.addEventListener("click", increaseCounter);
 minusCount.addEventListener("click", decreaseCounter);
 
-function increaseCounter(){
-     MrCount++;
-     counting.innerHTML = MrCount;
-     if(counting.innerHTML> "0"){
-          counting.style.color = "rgb(71, 29, 29)";
-     }
-     else if (counting.innerHTML === "0"){
-          counting.style.color = "grey"
-     }
-     counting.animate([{opacity:"0.5"},{opacity:"1.0"}],
-     {duration:700, fill:"forwards"})
+function increaseCounter() {
+  MrCount++;
+  counting.innerHTML = MrCount;
+  if (counting.innerHTML > "0") {
+    counting.style.color = "rgb(71, 29, 29)";
+  } else if (counting.innerHTML === "0") {
+    counting.style.color = "grey";
+  }
+  counting.animate([{ opacity: "0.5" }, { opacity: "1.0" }], {
+    duration: 700,
+    fill: "forwards",
+  });
 }
 
-function decreaseCounter(){
-     MrCount--;
-     counting.innerHTML = MrCount;
-     if(counting.innerHTML< "0"){
-          counting.style.color = "red"
-     }
-     else if (counting.innerHTML === "0"){
-          counting.style.color = "white"
-     }
-     counting.animate([{opacity:"0.5"},{opacity:"1.0"}],
-     {duration:700, fill:"forwards"})
+function decreaseCounter() {
+  MrCount--;
+  counting.innerHTML = MrCount;
+  if (counting.innerHTML < "0") {
+    counting.style.color = "red";
+  } else if (counting.innerHTML === "0") {
+    counting.style.color = "white";
+  }
+  counting.animate([{ opacity: "0.5" }, { opacity: "1.0" }], {
+    duration: 700,
+    fill: "forwards",
+  });
 }
